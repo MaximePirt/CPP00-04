9c957658ec91846ded7ba8be66e57aaf92d43477
Maxime <pierrot.maxime.pro@gmail.com>
2024-11-13 22:03:23 +0100

    Cpp02 ex0 ok, currently working on ex01

---
+++ Cpp02/ex01/Fixed.cpp
@@ -0,0 +1,32 @@
+#include "Fixed.hpp"
+
+Fixed::Fixed(){
+	this->fixed_point_number = 0;
+	std::cout << "Default constructor called" << std::endl;
+}
+
+Fixed::Fixed(const Fixed &old) : fixed_point_number(old.fixed_point_number){
+	std::cout << "Copy constructor called" << std::endl;
+}
+
+Fixed &Fixed::operator=(const Fixed &old) {
+	if (this != &old) {
+		fixed_point_number = old.fixed_point_number;
+		std::cout << "Copy assignment operator called" << std::endl;
+	}
+	return (*this);
+}
+
+int Fixed::getRawBits(void) const{
+	std::cout << "getRawBits member function called" << std::endl;
+	return (this->fixed_point_number);
+}
+
+void Fixed::setRawBits(int const raw){
+	this->fixed_point_number = raw;
+	std::cout << "Rawbits set" << std::endl;
+}
+
+Fixed::~Fixed() {
+	std::cout << "Fixed has been destroyed" << std::endl;
+}
---
+++ Cpp02/ex01/Fixed.hpp
@@ -0,0 +1,21 @@
+#ifndef FIXED_HPP
+#define FIXED_HPP
+
+#include <iostream>
+#include <cmath>
+
+class Fixed
+{
+	public:
+		Fixed();
+		Fixed(const Fixed &old);
+		Fixed &operator=(const Fixed &old);
+		~Fixed();
+		int getRawBits(void) const;
+		void setRawBits(int const raw);
+	private:
+		int fixed_point_number;
+		static const int fractional_bit_number = 8;
+};
+
+#endif
