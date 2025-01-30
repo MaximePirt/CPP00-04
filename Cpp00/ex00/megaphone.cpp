3c655829a5ce7d7f6cf56ec9d545e8431be2779c
Maxime <pierrot.maxime.pro@gmail.com>
2025-01-30 23:43:42 +0100

    modify ex00 when argc == 1

--- Cpp00/ex00/megaphone.cpp
+++ Cpp00/ex00/megaphone.cpp
@@ -15,13 +15,17 @@
 int main (int argc, char **argv)
 {
 	(void)argc;
+	if (argc == 1)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" <<  std::endl;
+		return (1);
+	}
 
 	for (int i = 1; i < argc; i++)
 	{
 		std::string str = argv[i];
 		for(int j = 0; str[j]; j++)
 			std::cout << (char)std::toupper(str[j]);
-		std::cout << " ";
 	}
 	std::cout << std::endl;
 	return (0);
